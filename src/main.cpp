23020cdfcdf64daf7aea279ce1a59d7e22617b4d
arshia8430 <pr.arshia@gmail.com>
2024-05-17 11:09:09 +0330

    rdkord

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,36 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
-#include <iostream>
-int main() {
-  sf::Window window(sf::VideoMode(800, 600), "game");
-  window.setVerticalSyncEnabled(true);
-  sf::Music music;
-  if (!music.openFromFile("music.ogg")) {
-    std::cerr << "music error" << std::endl;
-    return 1;
-  }
-  music.play();
-  sf::Image image;
-  if (!image.loadFromFile("image.png")) {
-    std::cerr << "load image error!" << std::endl;
-    return 1;
-  }
-  while (window.isOpen()) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed) 
-        window.close();
-    window.clear(sf::Color::Black);
-    window.display();
-  }
-  music.stop();
-  return 0;
+#include <string>
+using namespace sf;
+
+Sprite draw_image(RenderWindow window,string name,int x=-1,int y=-1){
+  Texture texture;
+  texture.loadFromFile(string+".jpg");
+  Sprite sprite;
+  if(x!=-1 && y!= -1)
+    sprite.setPostion(x,y);
+  return sprite;
+}
+int main()
+{
+    RenderWindow window(VideoMode(800, 600), "Game");
+    Texture texture;
+    texture.loadFromFile("Background_0.jpg");
+    Sprite wal(texture);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(sprite);
+        window.draw(draw_image(window,"card_wallnut",30,30));
+        window.display();
+    }
+
+    return 0;
 }
